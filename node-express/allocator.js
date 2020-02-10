9cce96c9a1e53d4c3e31534abb7f2850565601db
treywood-qz <trey.wood@quizlet.com>
2020-02-10 14:46:44 -0700

    comment class methods

--- node-express/allocator.js
+++ node-express/allocator.js
@@ -4,10 +4,13 @@ class Allocator {
   
   }
 
+  // given a server type ('API', 'DB', etc) return the name of the newly allocated
+  // server (e.g. 'API1', 'API2', 'DB5')
   allocate(serverType) {
 
   }
 
+  // given a server name, remove the server from the pool
   deallocate(serverName) {
     
   }
